9788206491e4802dfd9da2cec3d6c123803a8360
James Woodward <james@matoch.com>
2022-07-06 21:44:52 -0600

    Set line wrap and hide the showbreak character

--- lua/copymode.lua
+++ lua/copymode.lua
@@ -5,6 +5,8 @@ M.optList = nil
 M.optNumber = nil
 M.optRelativeNumber = nil
 M.optSigncolumn = nil
+M.showbreak = nil
+M.wrap = nil
 M.toggle = function()
     if M.cpmode == false then
         print("Copy Mode Enabled")
@@ -16,6 +18,10 @@ M.toggle = function()
         vim.opt.relativenumber = false
         M.optSigncolumn = vim.opt.signcolumn._value
         vim.opt.signcolumn = 'no'
+        M.showbreak = vim.opt.showbreak._value
+        vim.opt.showbreak = ''
+        M.wrap = vim.opt.wrap._value
+        vim.opt.wrap = true
         M.cpmode = true
     else
         print("Copy Mode Disabled")
@@ -23,6 +29,8 @@ M.toggle = function()
         vim.opt.number = M.optNumber
         vim.opt.relativenumber = M.optRelativeNumber
         vim.opt.signcolumn = M.optSigncolumn
+        vim.opt.showbreak = M.showbreak
+        vim.opt.wrap = M.wrap
         M.cpmode = false
     end
 end
